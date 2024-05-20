2b1816f1156bc522eb416766cc101506a71a86c9
babayaga-13 <muniruddinmahir@gmail.com>
2024-05-21 01:28:15 +0600

    done

--- Introductory Problems/Permutations.cpp
+++ Introductory Problems/Permutations.cpp
@@ -10,18 +10,19 @@ int main()
     ll n;
     cin >> n;
     vector<int> v;
-    if (n < 5)
+    if (n < 4 && n != 1)
         cout << "NO SOLUTION";
     else
     {
-        for (int i = 1; i <= n; i += 2)
-        {
-            v.push_back(i);
-        }
         for (int i = 2; i <= n; i += 2)
         {
             v.push_back(i);
         }
+        for (int i = 1; i <= n; i += 2)
+        {
+            v.push_back(i);
+        }
+
         for (auto u : v)
             cout << u << " ";
     }
